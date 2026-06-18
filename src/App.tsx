77347b9d7c47b4af731c9ba621eeaf3f38f54042
Mohammadreza <kashani.mohammadreza54@gmail.com>
2026-06-18 20:51:07 +0330

    Add: EditProfile component , Fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import RecommendedUsers from './components/RecommendedUsers'
 import ProfileCard from './components/ProfileCard'
 import CreatePosts from './components/CreatePosts'
 import Posts from './components/Posts'
+import Profile from './components/pages/Profile'
 
 
 
@@ -17,14 +18,17 @@ function App() {
       {/* <SignIn/>
       <SignUp/> */}
       <Header />
+      <div className='bg-background'>
       <div className="grid grid-cols-1 md:grid-cols-12 gap-6 p-4">
         <div className="hidden md:block md:col-span-3"><ProfileCard/></div>
-        <div className='md:col-span-6 space-y-6'><CreatePosts />
+        <div className='md:col-span-6 space-y-6'>
+        <Profile />
+          <CreatePosts />
           <Posts/>
         </div>
         <div className='hidden md:block md:col-span-3'> <RecommendedUsers/></div>
       </div> 
-      
+      </div>
        
       
       

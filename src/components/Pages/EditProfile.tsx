59760fb3eebabf3a64f6ffd336f2f8222891769e
Mohammadreza <kashani.mohammadreza54@gmail.com>
2026-06-18 18:33:43 +0330

    Fixing bugs

---
+++ src/components/Pages/EditProfile.tsx
@@ -0,0 +1,15 @@
+import ProfileCard from "../ProfileCard"
+
+
+function EditProfile() {
+  return (
+    <main className="bg-background min-h-[calc(100vh-77px)]">
+      <div className="flex justify-between">
+        <ProfileCard />
+            
+      </div>
+    </main>
+  );
+}
+
+export default EditProfile

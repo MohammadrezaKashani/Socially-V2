cde81099a72051cd3715e90fffa2a16d96550988
Mohammadreza <kashani.mohammadreza54@gmail.com>
2026-06-23 21:40:12 +0330

    Add : NoPost and Post and like component . Fix : background color

---
+++ src/components/NoPost.tsx
@@ -0,0 +1,12 @@
+
+
+function NoPost() {
+  return (
+    <div className="flex flex-col justify-start bg-neutral-300 rounded-lg py-1 px-2 gap-1 w-full">
+      <p className="font-semibold">There is no like</p>
+      <p className="text-sm">This user not likes any post</p>
+    </div>
+  )
+}
+
+export default NoPost
